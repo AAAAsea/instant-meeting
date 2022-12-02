f83b813cb5c92949590687a795574869c3e4d580
Asea <569389750@qq.com>
2022-12-02 22:22:51 +0800

    fix: 用户残留

--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import Create from '../../components/Create'
 import Join from '../../components/Join'
 import TopNavBar from '../../components/TopNavBar'
+import { SocketContext } from '../../contexts/SocketContext'
 
 const Room = () => {
   const [search, setSearch] = useSearchParams();
   const type = search.get('type');
+  const { setUsers } = useContext(SocketContext)
 
   const [value, setValue] = useState(type === 'create' ? 0 : 1)
+  useEffect(() => {
+    setUsers([]);
+  }, [])
 
   return (
     <div id="room">
