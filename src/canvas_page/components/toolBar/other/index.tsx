895116ae1864b88ca19b251b2f0bafc32b2d3759
Asea <569389750@qq.com>
2023-03-23 20:27:20 +0800

    feat: 退出批注前提醒

--- src/canvas_page/components/toolBar/other/index.tsx
+++ src/canvas_page/components/toolBar/other/index.tsx
@@ -12,12 +12,15 @@ import { PlayArrow } from "@mui/icons-material";
 import { Close } from "@mui/icons-material";
 import { Save } from "@mui/icons-material";
 import useDesktopCapture from "../../../hooks/useDesktopCapture";
+import { AlertDialog } from "../../../../components/MUI"
+import { useState } from "react";
 
 
 const OtherOperator = (props) => {
     const dispatcherContext = useContext(DispatcherContext);
-    const {className,setFold} = props;
-    const {pause, setPause} = useContext(PauseContext);
+    const { className,setFold } = props;
+    const { pause, setPause } = useContext(PauseContext);
+    const [ open, setOpen ] = useState(false);
 
     const quitCanvas = ()=>{
       window.electron.ipcRenderer.send("quitCanvas")
@@ -69,9 +72,23 @@ const OtherOperator = (props) => {
                     }
                   </Icon>
                 </span>
-                <span title="退出" ><Icon className="operator-item"><Close onClick={quitCanvas}/></Icon></span>
+                <span title="退出" >
+                  <Icon className="operator-item">
+                    <Close onClick={()=>{ setOpen(true); pauseCanvas(false); }}/>
+                  </Icon>
+                </span>
             </div>
             <span className="title">其他</span>
+            <AlertDialog
+              handleClose = {()=>{ setOpen(false) }}
+              open = {open}
+              content = "退出后批注将丢失，如有需要请先单击工具栏的保存后再退出。"
+              title = "退出批注"
+              handleConfirm = {quitCanvas}
+              handleCancel = {()=>{setOpen(false)}}
+              confirmTitle = "确认退出"
+              cancelTitle = "取消"
+            ></AlertDialog>
         </div>
     );
 };
